49206d8219b18fa05c432717ff7c3bc736bfdaf9
depioktvn <depioktvn@gmail.com>
2021-08-23 21:44:08 +0700

    Update data peserta

--- data-peserta.php
+++ data-peserta.php
@@ -33,7 +33,44 @@
     </nav>
 
     <!-- Optional JavaScript; choose one of the two! -->
+    <div class="container">
 
+<center>
+<h3 class="font-weight-bold mt-3 ml-4" >Halaman Data Peserta</h3>
+</center/>
+<table class="table table-bordered">
+<thead>
+<tr>
+<th scope="col">No</th>
+<th scope="col">Nama Peserta</th>
+<th scope="col">Nomer Telpn</th>
+<th scope="col">Asal Kota</th>
+<th scope="col">Aksi</th>
+</tr>
+</thead>
+<tbody>
+<?php 
+$i = 1;
+include "koneksi.php";
+$sql = "SELECT * FROM data";
+$query = mysqli_query($db,$sql);
+while($data = mysqli_fetch_array($query)){
+
+?>
+<tr>
+<th scope="row"><?php echo $i;?></th>
+<td><?php echo  $data['nama_peserta'];?></td>
+<td><?php echo  $data['no_tlpn'];?></td> 
+<td><?php echo  $data['asal_kota'];?></td>
+<td><a href="cetakpdf.php?id=<?php echo  $data['id_peserta'];?>" class="btn btn-warning">Cetak Sertifikat</button></td>
+</tr>
+<?php
+$i++;
+}
+?>
+</tbody>
+</table>
+</div>
     <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
