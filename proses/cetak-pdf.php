9eb4890aa8a5611f81279a1ec50663ca160b4f29
Coco-02734 <68150655+Coco-02734@users.noreply.github.com>
2021-08-25 10:41:27 +0700

    Merapikan Coding

--- proses/cetak-pdf.php
+++ proses/cetak-pdf.php
@@ -8,7 +8,7 @@ use Dompdf\Dompdf;
 $dompdf = new Dompdf();
 
 $id = $_GET['id'];
-$query = mysqli_query($db,"select * from data where id_peserta='$id'");
+$query = mysqli_query($db, "select * from data where id_peserta='$id'");
 $r_data = mysqli_fetch_array($query);
 $nama = $r_data['nama_peserta'];
 $tema = $r_data['tema'];
@@ -16,13 +16,33 @@ $tgl_acara = $r_data['tgl_acara'];
 $sebagai = $r_data['sebagai'];
 
 $html = '
-<!DOCTYPE html><html><head><link href="https://fonts.googleapis.com/css?family=Inter&display=swap" rel="stylesheet" /><link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet" /><link href="../assets/css/main.css" rel="stylesheet" /><title>Document</title></head>
-<body><center><div class="v4_15"><div class="v4_14"></div><span class="v1_56">KEMENTRIAN PENDIDIKAN DAN KEBUDAYAAN
-TEAM 3</span><span class="v1_57">SERTIFIKAT</span><span class="v4_2"> <center>No :'. rand() .'</center></span><span class="v1_58">Diberikan Kepada</span><span class="v1_60"> <center>'. $nama .'</center></span><div class="v1_61"></div><span class="v1_62">Atas peran serta sebagai ' . $sebagai .' dalam kegiatan webinar dengan tema "'. $tema .'" yang diselenggarakan oleh Gabungan dari peserta VSGA DTS JWD 2021, Diadakan pada tanggal '. $tgl_acara .'  
-
-
+<!DOCTYPE html>
+<html>
+<head>
+<link href="https://fonts.googleapis.com/css?family=Inter&display=swap" rel="stylesheet" />
+<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet" />
+<link href="../assets/css/main.css" rel="stylesheet" />
+<title>Document</title>
+</head>
+<body>
+<center>
+<div class="v4_15"><div class="v4_14"></div>
+<span class="v1_56">KEMENTRIAN PENDIDIKAN DAN KEBUDAYAAN
+TEAM 3</span>
+<span class="v1_57">SERTIFIKAT</span><span class="v4_2"> <center>No :' . rand()
+    . '</center>
 </span>
-<span class="v1_65">Ketua Panitia</span><span class="v2_92">Fernandes Yusril Al Depi Widi</span><div class="v7_18"></div></div></center></body></html>
+<span class="v1_58">Diberikan Kepada</span>
+<span class="v1_60"> <center>' . $nama . '</center></span>
+<div class="v1_61"></div>
+<span class="v1_62">Atas peran serta sebagai ' . $sebagai . ' dalam kegiatan webinar dengan tema "' . $tema . '" yang diselenggarakan oleh Gabungan dari peserta VSGA DTS JWD 2021, Diadakan pada tanggal ' . $tgl_acara . '  
+</span>
+<span class="v1_65">Ketua Panitia</span><span class="v2_92">Fernandes Yusril Al Depi Widi
+</span><div class="v7_18">
+</div></div>
+</center>
+</body>
+</html>
 ';
 
 
